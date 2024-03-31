71c97ab33cf29fb01013c53a74bedf96b353f3b7
devops4sure <rahulmaske8802@gmail.com>
2024-03-31 13:57:16 +0530

    bucket name & cluster name updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "vprahul24"
+    bucket = "bucketupto5kg"
     key    = "terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
